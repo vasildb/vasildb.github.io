1a842c84cb9d73418121110de78c55599d52cfd3
Vasil <vasildb@gmail.com>
2023-05-09 12:37:23 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="contact" element={<Contact />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
