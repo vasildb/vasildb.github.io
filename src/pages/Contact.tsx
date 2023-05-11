958cd2a5e175d4635a6eb2d4c574157931a0608e
Vasil <vasildb@gmail.com>
2023-05-11 17:50:16 +0200

    Improvements, fixes and content

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,31 +1,49 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 
 function Contact() {
 	const [message, setMessage] = useState("");
+	const [email, setEmail] = useState("");
 	const [sendMode, setSendMode] = useState("send");
+	const [subject, setSubject] = useState("");
 
 	const pickInterest = (interest: string) => {
 		let msg = "Hey Vasil, ";
+		let sub = "";
 
 		switch (interest) {
 			case "full-stack":
 				msg += "I need a full-stack dev to cover the back-end and front-end of my business website";
+				sub = "I need full-stack dev";
 				break;
 			case "mvp":
 				msg += "I want to create an MVP but I don't know where to start";
+				sub = "I need an MVP";
 				break;
 			case "new-business":
 				msg += "I'm starting a new business, and want to utilize the web to help me be more productive";
+				sub = "Make a web for my business";
 				break;
 			case "startup":
 				msg += "let's create the new unicorn";
+				sub = "Creating a startup";
 				break;
 			case "beer":
 				msg += "I don't have a clear idea, let's talk and see if we can collaborate";
+				sub = "Beer?";
 				break;
 		}
 
 		setMessage(msg + "...");
+		setSubject(sub);
+	};
+
+	const calcCurl = () => {
+		return ["curl --location 'https://vasil.nulius.com/contact' \\ ", "--header 'Content-Type: application/x-www-form-urlencoded' \\ ", "--data-urlencode 'email=" + email + "' \\ ", "--data-urlencode 'message=" + message + "'"];
+	};
+
+	const calcHTTP = () => {
+		const params = "email=" + encodeURI(email) + "&message=" + encodeURI(message);
+		return ["POST /contact HTTP/1.1", "Host: vasil.nulius.com", "Content-Type: application/x-www-form-urlencoded", "Content-Length: " + params.length, "", params];
 	};
 
 	return (
@@ -33,26 +51,49 @@ function Contact() {
 			<div className="titles">
 				I'm always interested in
 				<span onClick={() => pickInterest("full-stack")}>Full-Stack Development</span>
+				<span onClick={() => pickInterest("startup")}>Startups</span>
 				<span onClick={() => pickInterest("mvp")}>Minimum Viable Products</span>
 				<span onClick={() => pickInterest("new-business")}>New Businesses</span>
-				<span onClick={() => pickInterest("startup")}>Startups</span>
 				<span onClick={() => pickInterest("beer")}>A Beer</span>
 			</div>
 			<div className="text">
+				<input type="email" placeholder="Your email address" onChange={(e) => setEmail(e.target.value)} />
 				<textarea value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+				<div className="send">
+					<button className="btn">Send</button> &nbsp; or{" "}
+					<button className="btn link" onClick={() => setSendMode("http")}>
+						HTTP
+					</button>{" "}
+					or{" "}
+					<button className="btn link" onClick={() => setSendMode("curl")}>
+						cURL
+					</button>
+					or{" "}
+					<a href={"mailto:vasildb@gmail.com?subject=" + encodeURI(subject)} className="btn link">
+						email
+					</a>
+				</div>
 			</div>
-			<div className="send">
-				<button className="btn">Send</button> or{" "}
-				<button className="btn link" onClick={() => setSendMode("http")}>
-					HTTP
-				</button>{" "}
-				or{" "}
-				<button className="btn link" onClick={() => setSendMode("curl")}>
-					cURL
-				</button>
-			</div>
-			{sendMode === "http" && <div className="code">HTTP POST https://www.google.com/...</div>}
-			{sendMode === "curl" && <div className="code">curl https://www.google.com/...</div>}
+			{sendMode === "http" && (
+				<div className="code">
+					{calcHTTP().map((h, i) => (
+						<Fragment key={i}>
+							{h}
+							<br />
+						</Fragment>
+					))}
+				</div>
+			)}
+			{sendMode === "curl" && (
+				<div className="code">
+					{calcCurl().map((c, i) => (
+						<Fragment key={i}>
+							{c}
+							<br />
+						</Fragment>
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,110 @@
 function Home() {
-	return <div>Hello</div>;
+	return (
+		<div className="homepage">
+			<div className="bio">
+				<div className="info">
+					asd sa kfjhsd fksdfh sdkjfhsdasd sa kfjhsd fksdfh sdkjfhsdasd sa kfjhsd fksdfh sdkjfhsdasd sa kfjhsd fksdfh sdkjfhsdasd sa kfjhsd fksdfh sdkjfhsd
+					<br />
+					<br />
+					And in between, I swim, bike and&nbsp;
+					<a href="https://utmb.world/en/runner/4221680.vasil.zidrovski" target="_blank">
+						run
+					</a>
+					.
+				</div>
+			</div>
+			<div className="projects">
+				<div>
+					<div className="title">
+						CTO -
+						<a href="https://contentmutual.com" target="_blank">
+							ContentMutual.com
+						</a>
+						<span>Dec 2014 - Nov 2022</span>
+					</div>
+					<ul>
+						<li>Created the architecture of the platform with scaling in mind.</li>
+						<li>Setup the platform completely to Amazon Web Services, and led a small team.</li>
+						<li>Implemented NFT minting for authors, wrote a smart contract for escrow payments and integrated it seamlessly on the frontend.</li>
+					</ul>
+					<div className="technologies">
+						<span>Laravel</span>
+						<span>React</span>
+						<span>Nginx</span>
+						<span>Linux Servers</span>
+						<span>Web Sockets</span>
+						<span>ethers.js</span>
+						<span>Cypress</span>
+						<span>Hardhat</span>
+						<span>Solidity</span>
+					</div>
+				</div>
+				<div>
+					<div className="title">
+						Software Engineer -
+						<a href="https://staffconnect-app.com" target="_blank">
+							StaffConnect.net
+						</a>
+						<span>May 2019 - Nov 2021</span>
+					</div>
+					<ul>
+						<li>Worked on building a whole new multi server architecture for the backend while serving 500k users worldwide.</li>
+						<li>Included in every aspect of the platform - frontend, backend, web sockets, queues, mail servers, implementing new features and improving current ones.</li>
+						<li>Built a few smaller projects using many experimental technologies.</li>
+					</ul>
+					<div className="technologies">
+						<span>Laravel</span>
+						<span>Nginx</span>
+						<span>Angular</span>
+						<span>fabric.js</span>
+						<span>Caddy Server</span>
+					</div>
+				</div>
+				<div>
+					<div className="title">
+						CEO -
+						<a href="https://nulius.com" target="_blank">
+							Nulius.com
+						</a>
+						<span>Jun 2018 - Apr 2019</span>
+					</div>
+					<ul>
+						<li>Leading a team of developers and a designer, creating the high level design and coding standards.</li>
+						<li>Focused on user acquisition and improving the platform as well.</li>
+						<li>Speaking at local IT and motivational events to engage the community and promote the product.</li>
+					</ul>
+					<div className="technologies">
+						<span>Laravel</span>
+						<span>React</span>
+						<span>AWS</span>
+						<span>MySQL</span>
+						<span>Web Sockets</span>
+					</div>
+				</div>
+				<div>
+					<div className="title">
+						Team Lead -
+						<a href="https://www.grabit.io/" target="_blank">
+							GrabIT.mk
+						</a>
+						<span>Apr 2011 - Nov 2013</span>
+					</div>
+					<ul>
+						<li>Started as a web developer, working on e-Commerce and auction web platforms.</li>
+						<li>Led a small team working on a pricewatch engine, created the architecture of the platform, developed parsers, and duplicate detection algorithms.</li>
+						<li>Involved in project time estimations and organizing the teams for better performance.</li>
+					</ul>
+					<div className="technologies">
+						<span>PHP</span>
+						<span>Kohana</span>
+						<span>Apache</span>
+						<span>jQuery</span>
+						<span>Linux</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
