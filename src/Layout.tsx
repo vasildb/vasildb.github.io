5e8d28e46f59dcab6b3dc6c9456db8b924adc789
Vasil <vasildb@gmail.com>
2023-06-01 14:25:42 +0200

    Add upwork to footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -20,6 +20,9 @@ function Layout() {
 				<a href="https://www.linkedin.com/in/vasilz/" target="_blank" title="LinkedIn" aria-label="My LinkedIn Profile">
 					<div className="linkedin" />
 				</a>
+				<a href="https://www.upwork.com/freelancers/vzidrovski" target="_blank" title="Upwork" aria-label="My Upwork Profile">
+					<div className="upwork" />
+				</a>
 				<a href="https://www.zend-zce.com/en/yellow-pages/ZEND022160" target="_blank" title="Zend ZCE" aria-label="My Zend PHP Certification">
 					<div className="php" />
 				</a>
